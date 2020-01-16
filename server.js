2de361e3ca8255dc77e31d70abafefe775e3ae06
sebasacuna <sebastianacuna.it@gmail.com>
2020-01-16 17:59:28 -0300

    add compresion to node server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const compression = require('compression');
 
 console.log(__dirname);
 app.use(express.static(__dirname + '/dist/forms'));
@@ -8,5 +9,5 @@ app.use(express.static(__dirname + '/dist/forms'));
 app.get('/*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/dist/forms/index.html'));
 });
-
+app.use(compression());
 app.listen(process.env.PORT || 4000);
