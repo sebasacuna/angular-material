40c6ab4ed6079d3ac026f9dcab0ffd145d98be12
sebasacuna <sebastianacuna.it@gmail.com>
2020-01-16 15:45:55 -0300

    added resolve to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var gzip = require('gulp-gzip');
 
-gulp.task('compress', function() {
+gulp.task('compress', function(resolve) {
   gulp.src(['./dist/**/*.*'])
       .pipe(gzip())
-      .pipe(gulp.dest('./dist'));
+	  .pipe(gulp.dest('./dist'));
+  resolve();
 });
