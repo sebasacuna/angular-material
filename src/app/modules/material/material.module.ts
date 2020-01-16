cfb44a29ecee2edbd77e68f67e20661206830fda
sebasacuna <sebastianacuna.it@gmail.com>
2020-01-16 16:39:43 -0300

    optimize build

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -10,21 +10,18 @@ import {PortalModule} from '@angular/cdk/portal';
 import {ScrollingModule} from '@angular/cdk/scrolling';
 
 import {MatInputModule} from '@angular/material/input';
-import {MatCheckboxModule} from '@angular/material/typings/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatFormFieldModule} from '@angular/material';
-import {MatGridListModule} from '@angular/material/typings/grid-list';
-import {MatIconModule} from '@angular/material/typings/icon';
-import {MatSelectModule} from '@angular/material/typings/select';
-import {MatSnackBarModule} from '@angular/material/typings/snack-bar';
-import {MatListModule} from '@angular/material/typings/list';
-import {MatMenuModule} from '@angular/material/typings/menu';
-import {MatPaginatorModule} from '@angular/material/typings/paginator';
-import {MatSortModule} from '@angular/material/typings/sort';
-import {MatTableModule} from '@angular/material/typings/table';
-import {MatToolbarModule} from '@angular/material/typings/toolbar';
-import {MatTooltipModule} from '@angular/material/typings/tooltip';
+import {
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatListModule,
+  MatMenuModule,
+  MatPaginatorModule, MatSelectModule, MatSnackBarModule, MatSortModule, MatTableModule, MatToolbarModule, MatTooltipModule
+} from '@angular/material';
+
 
 
 @NgModule({
