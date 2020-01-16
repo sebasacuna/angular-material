041f4c05894459aaaf59a787e7d1ae5ee19cd9c5
sebasacuna <sebastianacuna.it@gmail.com>
2020-01-16 11:08:16 -0300

    add dates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+console.log(__dirname);
+app.use(express.static(__dirname + '/dist/forms'));
+
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/dist/forms/index.html'));
+});
+
+app.listen(process.env.PORT || 4000);
