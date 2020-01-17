7e49ccff9a4abfd71f1d9f4b6930333cda39a95b
sebasacuna <sebastianacuna.it@gmail.com>
2020-01-17 09:33:03 -0300

    delete service folder in replace of new project structure

--- server.js
+++ server.js
@@ -6,10 +6,33 @@ const compression = require('compression');
 console.log(__dirname);
 app.use(express.static(__dirname + '/dist/forms'));
 
-app.use(compression());
+
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
+
+/*app.use(compression({
+  // filter decides if the response should be compressed or not,
+  // based on the `shouldCompress` function above
+  filter: shouldCompress,
+  // threshold is the byte threshold for the response body size
+  // before compression is considered, the default is 1kb
+  threshold: 0
+}));*/
+app.use(compression({
+  filter: function () { return true; }
+}));
 
 app.get('/*', function(req, res) {
+  res.set('Content-Encoding', 'gzip');
 	res.sendFile(path.join(__dirname + '/dist/forms/index.html'));
+
 });
 
 app.listen(process.env.PORT || 4000);
